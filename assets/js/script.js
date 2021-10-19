2b92fa1666659317ab08fb03cc26907eeca974ca
Stefanie Buntz <stbuntz@gmail.com>
2021-10-20 09:44:29 +1030

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
+
 var cities = [];
+
+var cityForm=document.querySelector("#citySearch");
 var lookupCityInput=document.querySelector("#lookupCity");
 var weatherContainer=document.querySelector("#currentWeather");
 var citySearchInput = document.querySelector("#cityHistory");
 var forecastTitle = document.querySelector("#forecast");
-var forecastContainer= document.querySelector("#fivedayForecast");
+var forecastContainer = document.querySelector("#fivedayForecast");
 var pastSearchButton = document.querySelector("#searchHistory");
 
 var formSumbitHandler = function(event){
@@ -21,13 +24,9 @@ var formSumbitHandler = function(event){
     pastSearch(lookupCity);
 }
 
-
-
 var saveSearch = function(){
     localStorage.setItem("cities", JSON.stringify(cities));
 };
-
-
 
 var getCityWeather = function(lookupCity){
     var apiKey = "9d93230f3ad2bc78a7973c5234d7ba2e"
@@ -41,38 +40,37 @@ var getCityWeather = function(lookupCity){
     });
 };
 
-var displayWeather = function(weather, searchCity){
+var displayWeather = function(forecastWeather, searchCity){
   
    weatherContainer.textContent= "";  
    citySearchInput.textContent=  searchCity.charAt(0).toUpperCase() + searchCity.slice(1);
 
    var currentDate = document.createElement("h4")
-   currentDate.textContent= " " + moment(weather.dt.value).format("MMM D, YYYY") ;
+   currentDate.textContent= " " + moment(forecastWeather.dt.value).format("MMM D, YYYY") ;
    citySearchInput.appendChild(currentDate);
 
    var weatherIcon = document.createElement("img")
-   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${forecastWeather.weather[0].icon}@2x.png`);
    citySearchInput.appendChild(weatherIcon);
 
    var temperature = document.createElement("span");
-   temperature.textContent = "Temperature: " + weather.main.temp + " °C";
+   temperature.textContent = "Temperature: " + forecastWeather.main.temp + " °C";
    temperature.classList = "weatherInformation"
   
    var humidity = document.createElement("span");
-   humidity.textContent = "Humidity: " + weather.main.humidity + " %";
+   humidity.textContent = "Humidity: " + forecastWeather.main.humidity + " %";
    humidity.classList = "weatherInformation"
 
    var windSpeed = document.createElement("span");
-   windSpeed.textContent = "Wind Speed: " + weather.wind.speed + " m/s";
+   windSpeed.textContent = "Wind Speed: " + forecastWeather.wind.speed + " m/s";
    windSpeed.classList = "weatherInformation"
 
-   //append to container
    weatherContainer.appendChild(temperature);
    weatherContainer.appendChild(humidity);
    weatherContainer.appendChild(windSpeed);
 
-   var lat = weather.coord.lat;
-   var lon = weather.coord.lon;
+   var lat = forecastWeather.coord.lat;
+   var lon = forecastWeather.coord.lon;
    getUvIndex(lat,lon)
 }
 
@@ -82,14 +80,14 @@ var getUvIndex = function(lat,lon){
     fetch(apiURL)
     .then(function(response){
         response.json().then(function(data){
-            uvIndexColour(data)
+            displayUvIndex(data)
         });
     });
-    //console.log(lat);
-    //console.log(lon);
+    console.log(lat);
+    console.log(lon);
 }
  
-var uvIndexColour = function(index){
+var displayUvIndex = function(index){
     var uvIndex = document.createElement("div");
     uvIndex.textContent = "UV Index: "
     uvIndex.classList = "weatherInformation"
@@ -133,49 +131,47 @@ var display5Day = function(weather){
        var dailyForecast = forecast[i];
         
        
-       var forecastEl=document.createElement("div");
-       forecastEl.classList = "card FiveDayCardForecast";
+       var fivedayForcast=document.createElement("div");
+       fivedayForcast.classList = "card FiveDayCardForecast";
 
 
        var forecastDate = document.createElement("h3")
        forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM D, YYYY");
        forecastDate.classList = "card-header text-center"
-       forecastEl.appendChild(forecastDate);
+       fivedayForcast.appendChild(forecastDate);
 
 
        var weatherIcon = document.createElement("img")
        weatherIcon.classList = "card-body text-center";
        weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);  
 
-       forecastEl.appendChild(weatherIcon);
+       fivedayForcast.appendChild(weatherIcon);
        
-       var forecastTempEl=document.createElement("span");
-       forecastTempEl.classList = "card-body text-center";
-       forecastTempEl.textContent = dailyForecast.main.temp + " °C";
+       var forecastTemp=document.createElement("span");
+       forecastTemp.classList = "card-body text-center";
+       forecastTemp.textContent = dailyForecast.main.temp + " °C";
 
-        forecastEl.appendChild(forecastTempEl);
+        fivedayForcast.appendChild(forecastTemp);
 
-       var forecastHumEl=document.createElement("span");
-       forecastHumEl.classList = "card-body text-center";
-       forecastHumEl.textContent = dailyForecast.main.humidity + "  %";
+       var forecastHum=document.createElement("span");
+       forecastHum.classList = "card-body text-center";
+       forecastHum.textContent = dailyForecast.main.humidity + "  %";
 
-       forecastEl.appendChild(forecastHumEl);
-       forecastContainer.appendChild(forecastEl);
+       fivedayForcast.appendChild(forecastHum);
+       forecastContainer.appendChild(fivedayForcast);
     }
 
 }
 
 var pastSearch = function(pastSearch){
      
-    pastSearchEl = document.createElement("button");
-    pastSearchEl.textContent = pastSearch;
-    pastSearchEl.classList = "pastSearchButton";
-    pastSearchEl.setAttribute("data-city",pastSearch)
-    pastSearchEl.setAttribute("type", "submit");
+    pastSearch = document.createElement("button");
+    pastSearch.textContent = pastSearch;
+    pastSearch.classList = "pastSearchButton";
+    pastSearch.setAttribute("data-city",pastSearch)
+    pastSearch.setAttribute("type", "submit");
 
-
-
-    pastSearchButton.prepend(pastSearchEl);
+    pastSearchButton.prepend(pastSearch);
 }
 
 
@@ -187,5 +183,6 @@ var pastSearchHistory = function(event){
     }
 }
 
-cityFormEl.addEventListener("submit", formSumbitHandler);
+cityForm.addEventListener("submit", formSumbitHandler);
 pastSearchButton.addEventListener("click", pastSearchHistory);
+
